ffd2a803cbf05233989d9f4ac42092fcd568523b
iii <miselrkba@gmail.com>
2021-01-19 10:16:18 +0100

    added more test cases ran test --coverage --coverageReporters=text

--- index.test.js
+++ index.test.js
@@ -6,4 +6,9 @@ describe('fizzBuzz()', () => {
     expect(fizzBuzz(30)).toBe('FizzBuzz');
     expect(fizzBuzz(45)).toBe('FizzBuzz');
   });
+  it('returns the given number for multiples of neither 3 nor 5', ()=> {
+      expect(fizzBuzz(1)).toBe('1');
+      expect(fizzBuzz(22)).toBe('22');
+
+  })
 });
